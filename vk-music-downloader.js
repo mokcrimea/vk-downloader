bde074975285d58dd81fa671b028eb7b3995b9cb
Valentyn Khatsaiuk <mok.crimea@gmail.com>
2014-05-30 12:24:10 +0000

    Initial commit

---
+++ vk-music-downloader.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name MusicDownload from VK
+// @description Download music from vk.com
+// @author Valentine Khatsayuk
+// @license MIT
+// @version 0.0.1
+// @include http://vk.com/*
+// ==/UserScript==
+
+// wrap the script in a closure (opera, ie)
+// do not spoil the global scope
+// The script can be transformed into a bookmarklet easily :)
+(function(window, undefined) {
+
+    // normalized window
+    var w;
+    if (typeof unsafeWindow != undefined) {
+        w = unsafeWindow
+    } else {
+        w = window;
+    }
+
+    // You can inject almost any javascript library here.
+    // Just pass the w as the window reference,
+    // e.g. jquery.min.js embedding:
+    // (function(a,b){function ci(a) ... a.jQuery=a.$=d})(w);
+
+    // do not run in frames
+    if (w.self != w.top) {
+        return;
+    }
+
+    // additional url check.
+    if (/http:\/\/vk.com\/\w{6}\d*/.test(w.location.href)) {
+        var list = w.document.getElementsByClassName('play_new');
+        addGlobalStyle('.download_new { display: inline-block; padding-left: 20px; top: -15px; position: absolute; }');
+        for (var elem of list) {
+
+            console.log(elem);
+            var url = elem.parentNode.parentNode.children[1].value;
+            var urlParent = elem.parentNode.parentNode.children[0];
+
+            createIcon('empty', url, urlParent);
+        }
+    }
+
+    function createIcon(title, url, parent, onOk) {
+        var temp;
+        temp = w.document.createElement('a');
+        temp.className = 'download_new';
+        temp.href = 'url'
+        temp.innerHTML = 'Скачать';
+        parent.appendChild(temp);
+    }
+
+    function addGlobalStyle(css) {
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        if (!head) {
+            return;
+        }
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = css;
+        head.appendChild(style);
+    }
+})(window);
